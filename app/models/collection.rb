32fb0b8b60b8a4b1b9df631db16f4fa7315ae0a0
concordenzeyimana <nzeconco@gmail.com>
2022-12-06 12:09:57 -0500

    seed created for test

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,3 +1,3 @@
 class Collection < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Collection.destroy_all
+Vinyl.destroy_all
+User.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+user = User.create!(email: "concorde@gmail.com", password: "123456")
+artist = Artist.create!(name: "Wonder Woman 1984", description: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s")
+Collection.create!(name: "The Shawshank Redemption", user: user)
+genre = Genre.create!(name: "Titanic")
+Vinyl.create!(title: "Ocean's Eight", photo_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", year: 2001, artist:artist, genre:genre)
