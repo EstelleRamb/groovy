fb2331ed635ab48a97146dd073daceb46f5c903e
concordenzeyimana <nzeconco@gmail.com>
2022-12-08 17:47:51 -0500

    exchange

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,13 +1,14 @@
 class ExchangesController < ApplicationController
   def index
+    @exchanges = Exchange.where(user: current_user)
   end
 
   def create
     exchange = Exchange.new(requested_vinyl_id: params[:collections_vinyl_id], user: current_user, status: :pending)
     exchange.save!
     params[:collection_vinyl_ids].each do |cvid|
-      offered_vinyl = OfferedVinyl.new(exchange: exchange, collection_vinyl_id: cvid)
-      offered_vinyl.save!
+      offered_vinyls = OfferedVinyl.new(exchange: exchange, collection_vinyl_id: cvid)
+      offered_vinyls.save!
     end
     redirect_to my_exchanges_path
   # rescue
