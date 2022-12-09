1d305c9343f0407fc4a45cb3d26e97f40bd9b53c
edgar quenum <quenum.edgar@gmail.com>
2022-12-09 11:25:23 -0500

    essai changement id chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,6 @@
 class ChatroomsController < ApplicationController
     def show
-        @chatroom = Chatroom.find(params[:id])
+        @chatroom = Chatroom.find(params[:chatroom_id])
         @message = Message.new
     end
 end
