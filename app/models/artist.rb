7d362c50efacf1a3a516fa556bcfcd76edf96935
estelle rambur <estelle.rambur@gmail.com>
2022-12-09 11:23:27 -0500

    new seed and yelow button

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,5 @@ class Artist < ApplicationRecord
   has_many :vinyls
 
   validates :name, presence: true
-  validates :description, presence: true, length: { minimum: 10 }
+  # validates :description, presence: true
 end
