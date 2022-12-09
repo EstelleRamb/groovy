ca293cb695664bacf31e18df270458fb1ea1bb12
edgar quenum <quenum.edgar@gmail.com>
2022-12-09 12:12:00 -0500

    chat n'est plus lié a un id

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,6 @@
 class ChatroomsController < ApplicationController
     def show
-        @chatroom = Chatroom.find(params[:chatroom_id])
+        @chatroom = Chatroom.first
         @message = Message.new
     end
 end
