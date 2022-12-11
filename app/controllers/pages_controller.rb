0e60b0584bb4853e58a89baab888b563bed64a1e
estelle rambur <estelle.rambur@gmail.com>
2022-12-11 16:02:39 -0500

    add content in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @vinyl = Vinyl.find_by(title: "Doggystyle")
+    @vinylTwo = Vinyl.find_by(title: "Ben")
+    @vinylThree = Vinyl.find_by(title: "Queen")
+    @vinylFour = Vinyl.find_by(title: "Des Bises De Moi Pour Toi (From Me To You)")
   end
 end
