303946e6b2d36e947898890cf9633726ad89a4ef
estelle rambur <estelle.rambur@gmail.com>
2022-12-13 12:10:09 -0500

    Add a tab on collections index

--- app/models/users_vinyl.rb
+++ app/models/users_vinyl.rb
@@ -2,4 +2,16 @@ class UsersVinyl < ApplicationRecord
   belongs_to :user
   belongs_to :vinyl
   has_many :exchanges, class_name: 'Exchange', foreign_key: :requested_vinyl_id
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_year,
+    against: [ :vinyl_id],
+    associated_against: {
+      vinyl: [:title, :year]
+    },
+    using: {
+      tsearch: { prefix: true }
+
+    }
 end
