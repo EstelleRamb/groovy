d8a069266aca709c9dd1366eaf85f634e16e962d
concordenzeyimana <nzeconco@gmail.com>
2022-12-13 15:56:24 -0500

    fix issues with seeds file

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,11 +1,11 @@
 class Exchange < ApplicationRecord
   belongs_to :user
   belongs_to :requested_vinyl, class_name: "UsersVinyl", foreign_key: :requested_vinyl_id
-  has_many :offered_vinyls
+  has_many :offered_vinyls, dependent: :destroy
+  belongs_to :offered_vinyl, optional: true
   has_many :collections_vinyls, through: :offered_vinyls
   has_many :vinyls, through: :collections_vinyls
   has_many :messages, dependent: :destroy
-  belongs_to :offered_vinyl
 
   enum status: {
     pending: 0,
