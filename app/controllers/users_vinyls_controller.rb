457763e639f0c67f0362f68c0c315bb9dae47c7e
concordenzeyimana <nzeconco@gmail.com>
2022-12-15 10:16:46 -0500

    map adjusted

--- app/controllers/users_vinyls_controller.rb
+++ app/controllers/users_vinyls_controller.rb
@@ -1,14 +1,12 @@
 class UsersVinylsController < ApplicationController
-
   def create
-
     @users_vinyl = UsersVinyl.new
     @users_vinyl.user = current_user
     @vinyl = Vinyl.find(params[:vinyl_id])
     @users_vinyl.vinyl = @vinyl
     @users_vinyl.save
     # if @users_vinyl.save
-      redirect_to vinyls_path(query: params[:query]), notice: "#{@vinyl.title} a été ajouté à votre collection"
+    redirect_to vinyls_path(query: params[:query]), notice: "#{@vinyl.title} a été ajouté à votre collection"
     # else
     #   render :new, status: :unprocessable_entity
     # end
