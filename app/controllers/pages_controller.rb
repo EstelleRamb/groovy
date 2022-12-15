295fe7961dc6336ce4fb92a94e885be7c3aa78eb
estelle rambur <estelle.rambur@gmail.com>
2022-12-15 12:42:19 -0500

    fix carousel and update seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @vinyl = Vinyl.find_by(title: "Doggystyle")
-    @vinylTwo = Vinyl.find_by(title: "Ben")
-    @vinylThree = Vinyl.find_by(title: "Queen")
-    @vinylFour = Vinyl.find_by(title: "Des Bises De Moi Pour Toi (From Me To You)")
-    @vinylFive = Vinyl.find_by(title: "L'Aventurier")
-    @vinylSix = Vinyl.find_by(title: "I Don't Wanna Cry")
-    @vinylSeven = Vinyl.find_by(title: "Everybody Join Hands / Ain't No Sunshine")
-    @vinylEight = Vinyl.find_by(title: "Dizzidence Politik")
-    @vinylNine = Vinyl.find_by(title: "Maria (You Were The Only One)")
-    @vinylTen = Vinyl.find_by(title: "So High")
-    @vinylEleven = Vinyl.find_by(title: "Purrr!")
-    @vinylTwelve = Vinyl.find_by(title: "Mount Curve Avenue")
+    @vinyl = Vinyl.find_by(title: "The Dark side of the moon")
+    @vinylTwo = Vinyl.find_by(title: "A Night At The Opera")
+    @vinylThree = Vinyl.find_by(title: "Queen II")
+    @vinylFour = Vinyl.find_by(title: "Kind of blue")
+    @vinylFive = Vinyl.find_by(title: "Round about Midnight")
+    @vinylSix = Vinyl.find_by(title: "Fantaisie Militaire")
+    @vinylSeven = Vinyl.find_by(title: "Abbey road")
+    @vinylEight = Vinyl.find_by(title: "Rodeo")
+    @vinylNine = Vinyl.find_by(title: "Good kid, M.A.A.D city")
+    @vinylTen = Vinyl.find_by(title: "Ten")
+    @vinylEleven = Vinyl.find_by(title: "Comme d'habitude")
+    @vinylTwelve = Vinyl.find_by(title: "To pimp a butterfly")
   end
 end
